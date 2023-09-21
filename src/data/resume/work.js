14cf94681dfc348887359fde53703926182cf6e8
Automated <actions@users.noreply.github.com>
2023-09-20 23:48:17 -0400

    fixing date

--- src/data/resume/work.js
+++ src/data/resume/work.js
@@ -11,7 +11,8 @@ const work = [
   {
     name: 'Marcus Materials',
     position: 'Software Developer Intern',
-    startDate: '2021-09-01',
+    startDate: '2021-06-01',
+    endDate: '2021-08-31',
     highlights: [
       'Built a Spring Boot application which exposed helper calculation methods through REST endpoints. This application is currently used by employees to facilitate the writing of 3D printer scripts.',
     ],
