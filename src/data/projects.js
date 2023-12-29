c748e6f2e82dad7a36d6f15230ae4765f0305ea7
ErickAyon <145405207+ErickAyon@users.noreply.github.com>
2023-12-29 11:43:30 -0500

    Closing string

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,9 +5,7 @@ const data = [
     image: '/images/projects/yogaapp.jpg',
     date: '2023-09-01',
     desc:
-      'I love yoga and would love to start a donation based yoga app. Here is a backend I built using Spring Boot and Spring JPA. 
-      
-    You can find the code here: https://github.com/ErickAyon/yoga-class',
+      'I love yoga and would love to start a donation based yoga app. Here is a backend I built using Spring Boot and Spring JPA. You can find the code here: https://github.com/ErickAyon/yoga-class',
   },
 ];
 
